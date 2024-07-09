bad49f01255f819d49e970df7da70008b52d158c
Kostadina Geceska <kostadinag@nextsense.com>
2024-07-09 20:31:23 +0200

    verzija 1

---
+++ BossValidatorFramework.xcframework/ios-arm64/BossValidatorFramework.framework/Headers/BossValidatorFramework.h
@@ -0,0 +1,18 @@
+//
+//  BossValidatorFramework.h
+//  BossValidatorFramework
+//
+//  Created by Kostadina Gecevska on 9.7.24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BossValidatorFramework.
+FOUNDATION_EXPORT double BossValidatorFrameworkVersionNumber;
+
+//! Project version string for BossValidatorFramework.
+FOUNDATION_EXPORT const unsigned char BossValidatorFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BossValidatorFramework/PublicHeader.h>
+
+
